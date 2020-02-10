942c780f626438d1577410ba26591bdb5cb99b5f
Brian-P-Anderson <bpa@notionovus.com>
2020-02-10 04:23:48 -0600

    Changes made to Write Simulator

--- write_usb.py
+++ write_usb.py
@@ -7,10 +7,35 @@ from random import random
 counter=0
 seed(1)
 
+def my_function(rWt):
+  output = ''
+  rangePad = int(round(4 * random(), 0))
+  for x in range(6 + rangePad):
+#    output = output + str.encode(f'Z1G        0.00lb \r\n')
+    output = output + str(f'Z1G        0.00lb \r\n')
+  rangePad = int(round(4 * random(), 0))
+  for x in range(6 + rangePad):
+    incrWt = rWt / (6 + rangePad)
+    loopWt = round(incrWt * x, 2)
+#    output = output + str.encode(f' 1GM      {loopWt}lb \r\n')
+    output = output + str(f' 1GM      {loopWt}lb \r\n')
+  for x in range(6 + rangePad):
+#    output = output + str.encode(f' 1G        {rWt}lb \r\n')
+    output = output + str(f' 1G        {rWt}lb \r\n')
+  for x in range(6 + rangePad):
+    incrWt = rWt / (6 + rangePad)
+    loopWt = round(rWt - incrWt * x, 2)
+#    output = output + str.encode(f' 1GM      {loopWt}lb \r\n')
+    output = output + str(f' 1GM      {loopWt}lb \r\n')
+  for x in range(6 + rangePad):
+#    output = output + str.encode(f'Z1G        0.00lb \r\n')
+    output = output + str(f'Z1G        0.00lb \r\n')
+  return output
 
-ser = serial.Serial('/dev/ttyUSB1', 115200, 8, 'N', 1, timeout=1)
+# ser = serial.Serial('/dev/ttyUSB1', 115200, 8, 'N', 1, timeout=1)
 while True:
-  randomWt = round(33 + 40 * random(), 2)
-  ser.write(str.encode(f'Write counter: {counter}, Weight = {randomWt} \n'))
-  time.sleep(5)
+  randomWt = round(56 + 7 * random(), 2)
+#  ser.write(my_function(randomWt))
+  print(my_function(randomWt))
+  time.sleep(.05)
   counter += 1

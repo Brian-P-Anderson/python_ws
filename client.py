79828ec4733ff0620ded56eb2b959e2ea4730ff1
Brian-P-Anderson <bpa@notionovus.com>
2020-02-17 10:40:32 -0600

    Changes made to write_usb and client

--- client.py
+++ client.py
@@ -6,4 +6,6 @@ port = 12345
 s.connect(('127.0.0.1', port))
 
 while True:
-  print (s.recv(1024))
+  output = s.recv(1024)
+  if len(output):
+    print (s.recv(1024))
--- write_usb.py
+++ write_usb.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python
 import serial, time
+import socket
 from string import Template
 from random import seed
 from random import random
+
+s = socket.socket()
+
+port = 12345
+s.bind(('', port))
+
+s.listen(5)
 
 counter=0
 seed(1)
@@ -11,31 +19,32 @@ def my_function(rWt):
   output = ''
   rangePad = int(round(4 * random(), 0))
   for x in range(6 + rangePad):
-#    output = output + str.encode(f'Z1G        0.00lb \r\n')
     output = output + str(f'Z1G        0.00lb \r\n')
   rangePad = int(round(4 * random(), 0))
   for x in range(6 + rangePad):
     incrWt = rWt / (6 + rangePad)
     loopWt = round(incrWt * x, 2)
-#    output = output + str.encode(f' 1GM      {loopWt}lb \r\n')
     output = output + str(f' 1GM      {loopWt}lb \r\n')
   for x in range(6 + rangePad):
-#    output = output + str.encode(f' 1G        {rWt}lb \r\n')
     output = output + str(f' 1G        {rWt}lb \r\n')
   for x in range(6 + rangePad):
     incrWt = rWt / (6 + rangePad)
     loopWt = round(rWt - incrWt * x, 2)
-#    output = output + str.encode(f' 1GM      {loopWt}lb \r\n')
     output = output + str(f' 1GM      {loopWt}lb \r\n')
   for x in range(6 + rangePad):
-#    output = output + str.encode(f'Z1G        0.00lb \r\n')
     output = output + str(f'Z1G        0.00lb \r\n')
-  return output
+  print('Through my_function {output}')
+  return str.encode(output)
 
 # ser = serial.Serial('/dev/ttyUSB1', 115200, 8, 'N', 1, timeout=1)
 while True:
+  c, addr = s.accept()
+  print ('Got connection from', addr)
+
   randomWt = round(56 + 7 * random(), 2)
 #  ser.write(my_function(randomWt))
-  print(my_function(randomWt))
+
+  c.sendall(my_function(randomWt))
   time.sleep(.05)
   counter += 1
+  c.close()
